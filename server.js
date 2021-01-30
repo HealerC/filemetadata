abb569707a49fd4132ca0a4cb34d92818e1adbd5
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2021-01-30 23:20:56 +0100

    Completed project

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-const formidable = require('formidable');
+const formidable = require('formidable');	// Allows us to parse multipart data (file)
 
 var app = express();
 
@@ -11,9 +11,21 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+/* The app is simple. Get the file uploaded and respond with the name, size and type
+The app uses formidable to parse multipart data.
 
+May extend the app to accept multiple files and respond with each of them in an array */
 app.post('/api/fileanalyse', (req, res, next) => {
-	const form = formidable();
+	const form = formidable();				// Parses the file (can accept options)
+
+	form.parse(req, (err, fields, file) => {
+		if (err) {
+			console.error(err.message);
+			res.json("An error occured: " + err.name);
+		}
+		const { name, size, type } = file.upfile;
+		res.json({name, type, size});
+	})
 })
 
 
